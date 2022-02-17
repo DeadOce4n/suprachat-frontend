6c770d2be9554a44c98a0aa59a2c2c75299c0783
ThyDevourer <admin@suprachat.net>
2022-02-17 09:36:07 -0800

    Fixed 404 page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,43 +1,40 @@
 import React from 'react'
 import { navigate } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/Seo'
+import Container from '../components/Container'
+import Hero from '../components/Hero'
+import Button from '../components/Button'
 
 const NotFoundPage = () => {
   return (
-    <container>
-      <div className='container'>
-        <div className='split'>
-          <div className='hero notfound'>
-            <div className='hero-text'>
-              <h1>Oops!</h1>
-              <p>
-                Lo sentimos, la página que buscas no existe{' '}
-                <span role='img' aria-label='Pensive emoji'>
-                  😔
-                </span>{' '}
-                Pero no te desanimes, haz click en el botón de abajo para volver
-                al inicio, o en cualquiera de los enlaces de la barra superior
-                para ir a alguna otra página.
-              </p>
-              <button
-                onClick={() => navigate('/')}
-                style={{ width: '100%', textAlign: 'center' }}
-              >
-                Ir al inicio
-              </button>
-            </div>
-          </div>
-          <div className='img-container'>
-            <StaticImage
-              src='../images/empty.jpeg'
-              alt='Vacío'
-              title='En esta página no hay nada'
-              placeholder='blurred'
-            />
-          </div>
-        </div>
-      </div>
-    </container>
+    <>
+      <section>
+        <Container className='split'>
+          <Hero>
+            <h1 style={{ alignSelf: 'start' }}>Oops!</h1>
+            <p>
+              Lo sentimos, la página que buscas no existe{' '}
+              <span role='img' aria-label='Pensive emoji'>
+                😔
+              </span>{' '}
+              Pero no te desanimes, haz click en el botón de abajo para volver
+              al inicio, o en cualquiera de los enlaces de la barra superior
+              para ir a alguna otra página.
+            </p>
+            <Button onClick={() => navigate('/')} primary>
+              Ir al inicio
+            </Button>
+          </Hero>
+          <StaticImage
+            src='../images/empty.jpeg'
+            alt='Vacío'
+            title='En esta página no hay nada'
+            placeholder='blurred'
+          />
+        </Container>
+      </section>
+    </>
   )
 }
 
