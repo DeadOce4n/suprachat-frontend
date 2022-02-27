37563346ca520b955cb6582420f6590b6e42a909
ThyDevourer <admin@suprachat.net>
2022-02-27 09:31:10 -0800

    Improved notification layout

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -11,14 +11,21 @@ const StyledNotification = styled.div`
   left: 50%;
   transform: translateX(-50%);
   z-index: 10;
-  height: 6.5rem;
+  min-height: 6.5rem;
   font-size: 0.9em;
   font-weight: 700;
-  padding: 0 2rem;
   margin: 1.5rem 0 0 0;
+  padding: 1rem 2rem;
   border-radius: 10px;
   color: var(--color-bg-${props => props.theme.theme});
   background-color: var(--color-success-${props => props.theme.theme});
+  @media only screen and (min-width: 40em) {
+    width: calc(20em);
+    top: 6.5rem;
+    left: initial;
+    right: 0;
+    transform: translateX(-1.5rem);
+  }
 `
 
 const ErrorNotification = styled(StyledNotification)`
