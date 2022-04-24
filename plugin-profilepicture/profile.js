ba4effd8def94a71d4b8b796424b6a8f672803ab
ThyDevourer <admin@suprachat.net>
2022-04-24 13:58:08 -0700

    Updated kiwi plugin

--- plugin-profilepicture/profile.js
+++ plugin-profilepicture/profile.js
@@ -1,29 +1,42 @@
 /* global kiwi:true */
 const baseUrl = kiwi.state.getSetting('settings.profilePlugin.apiUrl')
 
+const getPicture = async username => {
+    const response = await fetch(`${baseUrl}/users/${username}`)
+    const data = await response.json()
+    return data.picture || "default.png"
+}
+
 kiwi.plugin('profile', (kiwi, log) => {
   kiwi.on('irc.userlist', (event, network) => {
-    event.users.forEach((user) => {
+    event.users.forEach(async (user) => {
       const nick = user.nick
       const account = user.account || nick
+
+      const picture = await getPicture(account)
 
       kiwi.state.addUser(network.id, {
         nick,
         avatar: {
-          small: `${baseUrl}/users/${account}/picture`
+          small: `${baseUrl}/upload/${picture}`
         }
       })
+
     })
   })
-  kiwi.on('irc.join', (event, network) => {
+
+  kiwi.on('irc.join', async (event, network) => {
     const nick = event.nick
     const account = event.account || nick
+
+    const picture = await getPicture(account)
 
     kiwi.state.addUser(network.id, {
       nick,
       avatar: {
-        small: `${baseUrl}/users/${account}/picture`
+        small: `${baseUrl}/upload/${picture}`
       }
     })
   })
+
 })
